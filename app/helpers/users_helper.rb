f1a7686924ab34444ea3ed7feb8ef85c8c411a92
tktk <tktk.stereoman@gmail.com>
2015-04-19 12:11:01 +0900

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravator_for(user)
+    gravator_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravator_url = "http//secure.gravator.com/avatar/#{gravator_id}"
+    image_tag(gravator_url, alt: user.name, class: "gravator")
+  end
 end
